72247d740bebc631a429c0ab83180c10ebfe0106
Ricardo Junior <ricardodev10@yahoo.com>
2022-11-17 17:40:32 -0300

    :sparkles: feat: add imask and regex

--- src/main.js
+++ src/main.js
@@ -1,1 +1,100 @@
-import "./css/index.css"
+import "./css/index.css";
+import IMask from "imask";
+
+const ccBgColor01 = document.querySelector(
+  ".cc-bg svg > g g:nth-child(1) path"
+);
+const ccBgColor02 = document.querySelector(
+  ".cc-bg svg > g g:nth-child(2) path"
+);
+
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img");
+
+function setCardType(type) {
+  const colors = {
+    visa: ["#436D99", "#2D57F2"],
+    mastercard: ["#C69347", "#DF6F29"],
+    americanExpress: ["#439946", "#2DF235"],
+    hipercard: ["#784399", "#B32DF2"],
+    elo: ["#994343", "#F22D2D"],
+    default: ["black", "gray"],
+  };
+
+  ccBgColor01.setAttribute("fill", colors[type][0]);
+  ccBgColor02.setAttribute("fill", colors[type][1]);
+
+  ccLogo.setAttribute("src", `cc-${type}.svg`);
+}
+// setCardType("");
+
+const securityCode = document.querySelector("#security-code");
+const securityCodePattern = {
+  mask: "0000",
+};
+const securityCodeMasked = IMask(securityCode, securityCodePattern);
+
+const expirationDate = document.querySelector("#expiration-date");
+const expirationDatePattern = {
+  mask: "MM{/}YY",
+  blocks: {
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12,
+    },
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 10).slice(2),
+    },
+  },
+};
+const expirationDateMasked = IMask(expirationDate, expirationDatePattern);
+
+// Visa: ^4[0-9]{15}$
+// Mastercard: ^((5(([1-2]|[4-5])[0-9]{8}|0((1|6)([0-9]{7}))|3(0(4((0|[2-9])[0-9]{5})|([0-3]|[5-9])[0-9]{6})|[1-9][0-9]{7})))|((508116)\\d{4,10})|((502121)\\d{4,10})|((589916)\\d{4,10})|(2[0-9]{15})|(67[0-9]{14})|(506387)\\d{4,10})
+
+const cardNumber = document.querySelector("#card-number");
+const cardNumberPattern = {
+  mask: [
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^4\d{0,15}/,
+      cardtype: "visa",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
+      cardtype: "mastercard",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^3[47]\\d{0,13}/,
+      cardtype: "american-express",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^606282|^3841(?:[0|4|6]{1})0/,
+      cardtype: "hipercard",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex:
+        /^(4011(78|79)|43(1274|8935)|45(1416|7393|763(1|2))|50(4175|6699|67[0-7][0-9]|9000)|50(9[0-9][0-9][0-9])|627780|63(6297|6368)|650(03([^4])|04([0-9])|05(0|1)|05([7-9])|06([0-9])|07([0-9])|08([0-9])|4([0-3][0-9]|8[5-9]|9[0-9])|5([0-9][0-9]|3[0-8])|9([0-6][0-9]|7[0-8])|7([0-2][0-9])|541|700|720|727|901)|65165([2-9])|6516([6-7][0-9])|65500([0-9])|6550([0-5][0-9])|655021|65505([6-7])|6516([8-9][0-9])|65170([0-4]))/,
+      cardtype: "elo",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      cardtype: "default",
+    },
+  ],
+  dispatch: function (appended, dynamicMasked) {
+    const number = (dynamicMasked.value + appended).replace(/\D/g, "");
+    const foundMask = dynamicMasked.compiledMasks.find(function (item) {
+      return number.match(item.regex);
+    });
+
+    return foundMask;
+  },
+};
+const cardNumberMasked = IMask(cardNumber, cardNumberPattern);
